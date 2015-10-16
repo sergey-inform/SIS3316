a1f89876f45b94989d5ec725f7610e44cb3992b1
Sergey Ryzhikov <sergey-inform@ya.ru>
2015-10-17 02:00:29 +0300

    parse.py tmp: ctypes format, need to rethink the algorithm

--- tools/parse.py
+++ tools/parse.py
@@ -5,7 +5,7 @@ import sys,os
 import argparse
 from struct import unpack
 import io # BufferedReader
-from ctypes import *
+import ctypes 
 
 __fields__ = ('npeak','peak','info',
 	'acc1','acc2','acc3','acc4','acc5','acc6','acc7','acc8',
@@ -49,27 +49,108 @@ class Parse:
 	def __iter__(self):
 		return self
 		
-	def _parse_event_format(self, fmt):
-		""" Try to calculate format of the next event.
-		Accept a value of 
-		Return a ctypes format or raise ValueError/StopIteration. """
+	def _parse_event_format(self): 
+		""" Try to calculate format of the next event in _reader.
+		Return a ctypes structure or raise ValueError/StopIteration.
+		"""
 		# Since Raw data format is a bit "overoptimized"... 
-		# We don't know event size for channels in each group, and there is no MAW length field,
-		# so it's not easy to calculate actual event length looking on data...
-		#
-		# Reasonable workaround is to try to find next higher timestamp value, since it changes
-		# only once in 17 seconds on 250 MHz.
+		# We don't know event size for channels in each group, so we need to calculate it on data.
 		
-		return None
+		MAX_HDR_LEN = 18 * 4
+		header = self._reader.peek(MAX_HDR_LEN)
+		c_format = [
+				("ts_hi", ctypes.c_int16),
+				("chan", ctypes.c_int, 12),
+				("fmt", ctypes.c_int, 4),
+				("ts_lo1", ctypes.c_int16),
+				("ts_lo2", ctypes.c_int16)]
+		try:
+			ts_hi, ch_fmt = unpack('<HH', header[0:4] )
+			ch, fmt = ch_fmt >>4, ch_fmt & 0xF
+			pos = 8 #raw data header position
+			
+			if fmt & 0b1:
+				pos += 7 * 4
+				c_format.extend([
+						('npeak', ctypes.c_int16),
+						('peak', ctypes.c_int16),
+						('info', ctypes.c_int8),
+						('acc1', ctypes.c_int32, 24),
+						('acc2', ctypes.c_int32),
+						('acc3', ctypes.c_int32),
+						('acc4', ctypes.c_int32),
+						('acc5', ctypes.c_int32),
+						('acc6', ctypes.c_int32),
+						])
+			if fmt & 0b10:
+				pos += 2 * 4
+				c_format.extend([
+						('acc7', ctypes.c_int32),
+						('acc8', ctypes.c_int32),
+						])
+			if fmt & 0b100:
+				pos += 3 * 4
+				c_format.extend([
+						('maw_max', ctypes.c_int32),
+						('maw_after_trig', ctypes.c_int32),
+						('maw_before_trig', ctypes.c_int32),
+						])
+			if fmt & 0b1000:
+				pos += 2 * 4
+				c_format.extend([
+						('e_start', ctypes.c_int32),
+						('e_max', ctypes.c_int32),
+						])
+			
+			hdr_raw = unpack('<I', header[pos:pos+4] )[0]
+			OxE, fMAW, n_raw = hdr_raw >> 28, bool(hdr_raw & (1<<27)), 2 * (hdr_raw & 0x1FFffFF)
+			n_avg = 0
+			
+			if OxE == 0xA: #additional Average Data header
+				hdr_avg = unpack('<I', header[pos+4:pos+8] )[0]
+				OxE, n_avg = hdr_avg >> 28, 2 * (hdr_avg & 0xFFff)
+				
+				if OxE != 0xE: 
+					raise ValueError('no 0xE after 0xA')
+		
+			elif OxE != 0xE:
+				raise ValueError('no 0xE')
+			
+			if n_raw:
+				c_format.append( ('raw', ctypes.c_int16 * n_raw) )
+					
+			if n_avg:
+				c_format.append( ('avg', ctypes.c_int16 * n_avg) )
+			
+			
+			# There is no MAW length field :`(,
+			# so it's not easy to calculate actual event length looking on data...
+			#
+			# Reasonable workaround is to try to find next higher timestamp value, since it changes
+			# only once in 17 seconds on 250 MHz.
+			#
+			if fMAW:
+				#TODO: look up for the next (timestamp +- 1)
+				pass
+			
+		except IndexError:
+			print('_parse_event_format: Index Error')
+			raise EOF
+		
+		# build a ctypes structure class
+		class CtypesStruct(ctypes.Structure):
+			_fields_ = c_format
+		CtypesStruct.__name__ = 'ch' + str(ch)
+		
+		return CtypesStruct
 		
 		
-		
-	def _peek_event(self, format_, fields):
+	def _peek_event(self, format_):
 		
 		if not format_:
 			raise ValueError("no format")
 		
-		pass
+		return {sz:140}
 		
 	def _cache_lookup(self, key):
 		return None
@@ -82,41 +163,70 @@ class Parse:
 		fields = self._fields
 		
 		data = []
-		
-		try:
-			while True:
-				print reader.tell()
+		evt_format = None
+		prev_ch_fmt = None
+	
+		while True:
+			try:
 				header = reader.peek(4)
 				if len(header) < 4:
-					raise EOFError #no more data
+					raise EOFError
 				
-				evt_format = self._cache_lookup(header)
+				ch_fmt = header[2:4]
+				
+				if ch_fmt != prev_ch_fmt: #next channel data, or format has changed
+					if evt_format:
+						# self._cache_update(header, evt_format)
+						pass
+					evt_format = self._cache_lookup(header)
 				
 				try:
-					evt = self._peek_event(evt_format, fields)
-				
-				except ValueError as e: #bad format
-					try:
-						evt_format = self._parse_event_format(header)
+					evt = self._peek_event(evt_format)
 					
-					except ValueError: #bad data
-						reader.read(1) #skip 1 byte
+				except ValueError: # evt_format is None, or it doesn't match the data?
+					try:
+						evt_format = self._parse_event_format()
+						continue
+						
+					except ValueError: #can't parse a new format. bad data?
+						reader.read(1) #skip 1 byte, maybe further data is ok
+						print 'bad data' #DELME:
 						continue
 				
+				#OK
+				prev_header = header
+				data.append(evt)
+				reader.read(evt.sz) #move forward
+				continue
 				
-				if evt_format:
-					for field_name, field_type in evt_format._fields_:
-						print field_name#, getattr(evt_format, field_name)
 				
-				
-				
-		except EOFError:
-			if data:
-				return data
-			else:
-				raise StopIteration
 			
+					
+			
+			except EOFError:
+				if data:
+					#TODO: sort data by ts
+					return data
+				else:
+					raise StopIteration
+			
+			
+		print "Err: we should never get here"
 		return data
+			
+				
+				except ValueError as e: 
+					try:
+						
+						
+						#~ print evt_format
+					
+					
+				
+				prev_header = header
+				
+				
+		
 	
 	__next__ = next
 	
