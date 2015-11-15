d138623f5f7218b77c1045dc8a01aa26823f34e9
Sergey Ryzhikov <sergey-inform@ya.ru>
2015-11-15 18:15:21 +0300

    parse.py 1st working version

--- tools/parse.py
+++ tools/parse.py
@@ -50,6 +50,9 @@ class PeekableObject(object):
 			
 	def skip(self, size):
 		''' Shrink internal buffer. '''
+		if size > len(self.buf): #TODO: refactor
+			self.peek(size - len(self.buf))
+			
 		self.pos += size
 		self.buf = self.buf[size:]
 
@@ -78,9 +81,177 @@ class Parse:
 			raise ValueError('You are trying to read data from a terminal.')
 		
 		self._reader = PeekableObject(fileobj)
-
-
-
+	
+	def __iter__(self):
+		return self
+	
+	def next(self):
+		""" Return events from a continuous redout (in general, from single memory bank), ordered by timestamp.
+		When events are rare, consecutive readouts could be merged. """
+		reader = self._reader
+		fields = self._fields
+		
+		while True: # until next event parsed successfully or EOF 
+			evt = None
+			evt_format = None
+			
+			try:
+				evt_format = self._parse_next()
+				#DELME:
+				#~ for a in evt_format._fields_:
+					#~ print(a[0], getattr (evt_format, a[0]))
+				evt = self._peek_next(evt_format)
+				
+			except ValueError as e:
+				print ('skip4 %s, pos:%d, data:%s' % (str(e), reader.pos, reader.peek(26).encode('hex')) )
+				reader.skip(4) #skip 4 bytes, maybe further data is ok
+				continue
+				
+			except EOFError:
+				raise StopIteration
+			
+			if evt:
+				reader.skip(evt.sz) #move forward
+				return evt
+			else:
+				return None
+				
+		#TODO: check a timestamp of a next event
+	
+	def _parse_next(self): 
+		""" Try to calculate format of the next event from _reader.
+		Return a ctypes structure or raise ValueError/StopIteration.
+		"""
+		# Since a raw data format is a bit "overoptimized" 
+		# we don't know true event sizes, so we need to calculate it on data.
+		
+		MAX_HDR_LEN = 18 * 4
+		header = self._reader.peek(MAX_HDR_LEN)
+		
+		print ('_parse_event_format header', header[0:20].encode('hex'))
+		
+		c_format = [
+				("fmt", ctypes.c_uint, 4),
+				("chan", ctypes.c_uint, 12),
+				("ts_hi", ctypes.c_uint, 16),
+				("ts_lo2", ctypes.c_uint16),
+				("ts_lo1", ctypes.c_uint16),
+				]
+		try:
+			ch_fmt, ts_hi = unpack('<HH', header[0:4] )
+			ch, fmt = ch_fmt >> 4, (ch_fmt & 0xF)
+			pos = 8 #raw data header position
+			
+			if fmt & 0b1:
+				pos += 7 * 4
+				c_format.extend([
+						('npeak', ctypes.c_int16),
+						('peak', ctypes.c_int16),
+						('info', ctypes.c_int8),
+						('acc1', ctypes.c_int32, 24),
+						('acc2', ctypes.c_int32),
+						('acc3', ctypes.c_int32),
+						('acc4', ctypes.c_int32),
+						('acc5', ctypes.c_int32),
+						('acc6', ctypes.c_int32),
+						])
+			if fmt & 0b10:
+				pos += 2 * 4
+				c_format.extend([
+						('acc7', ctypes.c_int32),
+						('acc8', ctypes.c_int32),
+						])
+			if fmt & 0b100:
+				pos += 3 * 4
+				c_format.extend([
+						('maw_max', ctypes.c_int32),
+						('maw_after_trig', ctypes.c_int32),
+						('maw_before_trig', ctypes.c_int32),
+						])
+			if fmt & 0b1000:
+				pos += 2 * 4
+				c_format.extend([
+						('e_start', ctypes.c_int32),
+						('e_max', ctypes.c_int32),
+						])
+			
+			hdr_raw = unpack('<I', header[pos:pos+4] )[0]
+			OxE, fMAW, n_raw = hdr_raw >> 28,  bool(hdr_raw & (1<<27)),  2 * (hdr_raw & 0x1FFffFF)
+			pos += 4
+			c_format.append( ('hdr_raw', ctypes.c_uint32))
+			
+			n_avg = 0
+			
+			if OxE == 0xA: #additional Average Data header
+				hdr_avg = unpack('<I', header[pos+4:pos+8] )[0]
+				OxE, n_avg = hdr_avg >> 28, 2 * (hdr_avg & 0xFFff)
+				
+				if OxE != 0xE: 
+					raise ValueError('no 0xE after 0xA')
+				
+				pos += 4
+				c_format.append( ('hdr_avg', ctypes.c_uint32))
+		
+			elif OxE != 0xE:
+				raise ValueError('no 0xE')
+			
+			if n_raw:
+				pos += 2 * n_raw
+				c_format.append( ('raw', ctypes.c_int16 * n_raw) )
+					
+			if n_avg:
+				pos += 2 * n_avg
+				c_format.append( ('avg', ctypes.c_int16 * n_avg) )
+			
+			
+			# There is no MAW length field :`(,
+			# so it's not easy to calculate actual event length looking on data...
+			#
+			# A reasonable workaround is to try to find next higher timestamp value, since it changes
+			# only once in 17 seconds (on 250 MHz), and assume that MAW is everything between the pos
+			# and the next timestamp.
+			#
+			if fMAW:
+				#TODO: look up for the next timestamp ( or timestamp + 1)
+				#n_maw=...
+				#~ pos += 4 * n_maw
+				#c_format.append( ('maw', ctypes.c_int32 * n_maw) )
+				pass
+			
+		except struct_error:
+			# occures than len(header[slice]) is less then expected
+			raise EOFError
+		
+		# build a ctypes structure class
+		class CtypesStruct(ctypes.LittleEndianStructure):
+			_pack_ = 1 #align bitfields without gaps
+			_fields_ = c_format
+		CtypesStruct.__name__ = 'ch' + str(ch)
+		
+		return CtypesStruct
+	
+	def _peek_next(self, format_):
+		""" Interprete bytes from _reader according to format_
+		"""
+		if not format_:
+			raise ValueError("no format")
+		
+		sz = ctypes.sizeof(format_) #estimated length
+		data = self._reader.peek(sz)
+		evt = format_.from_buffer_copy(data) #raises ValueError if not enougth data
+		evt.sz = sz
+		
+		#check 0 blocks
+		for a in ['acc1','acc2','acc3','acc4','acc5','acc6','acc7','acc8',
+	'maw_max','maw_after_trig','maw_before_trig']:
+			if hasattr(evt, a) and getattr(evt, a) >= (1<<28):
+				raise ValueError("wrong value of %s" % a)
+		
+		return evt
+	
+	__next__ = next
+	
+	
 def main():
 	parser = argparse.ArgumentParser(description=__doc__)
 	parser.add_argument('infile', nargs='?', type=str, default='-',
@@ -111,9 +282,10 @@ def main():
 		sys.stderr.write("Err: %s \n" % e)
 		exit(1)
 	
-	for events in p:
-		pass
-		#~ print(events)
+	for event in p:
+		for f in event._fields_:
+			print  f[0], getattr(event, f[0])
+		print len(event.raw)
 	
 	
 if __name__ == "__main__":
